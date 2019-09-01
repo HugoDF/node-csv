82efcce487b604565573f7822a1ccfed34e8ac8a
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-09-01 21:05:47 +0100

    add naive CSV parser + tests

---
+++ csv-parser.js
@@ -0,0 +1,30 @@
+const {EOL} = require('os');
+
+const getValues = csv => csv.split(',');
+const getRows = csv => csv.split(EOL);
+
+function csvParser(csvText) {
+  if (!csvText) {
+    return [];
+  }
+
+  const [firstLine, ...lines] = getRows(csvText);
+
+  const headingEntries = getValues(firstLine);
+  const nameByColumnNumber = headingEntries.reduce((acc, curr, idx) => {
+    acc[idx] = curr;
+    return acc;
+  }, {});
+  const columnCount = headingEntries.length;
+
+  return lines.map(csv => {
+    return getValues(csv).reduce((acc, curr, idx) => {
+      if (idx > columnCount) return acc;
+      const fieldName = nameByColumnNumber[idx];
+      acc[fieldName] = curr;
+      return acc;
+    }, {});
+  });
+}
+
+module.exports = csvParser;
---
+++ csv-parser.test.js
@@ -0,0 +1,58 @@
+const test = require('tape');
+const csvParser = require('./csv-parser');
+
+test('should support empty string', async t => {
+  t.plan(1);
+  const actual = await csvParser('');
+  t.deepEqual(actual, [], 'returns [] when passed ""');
+});
+
+const singleLineCsv = `Title,Other,Values`;
+
+test('should support title line', async t => {
+  t.plan(1);
+  const actual = await csvParser(singleLineCsv);
+  t.deepEqual(actual, [], 'returns []');
+});
+
+const multilineCsv = `title,other,value
+hello world,what,value`;
+
+test('should parse standard entry', async t => {
+  t.plan(1);
+  const actual = await csvParser(multilineCsv);
+  const expected = [{title: 'hello world', other: 'what', value: 'value'}];
+  t.deepEqual(actual, expected);
+});
+
+
+const extraColumnsCsv = `title,other,value
+hello world,what,value,extra,value`;
+
+test('should ignore extra columns', async t => {
+  t.plan(1);
+  const actual = await csvParser(multilineCsv);
+  const expected = [{title: 'hello world', other: 'what', value: 'value'}];
+  t.deepEqual(actual, expected);
+});
+
+const quoteCommaCsv = `title,other,value
+"hello "comma", "world"",what,value`;
+
+test.skip('should handle quoted commas', async t => {
+  t.plan(1);
+  const actual = await csvParser(multilineCsv);
+  const expected = [{title: 'hello "comma", "world"', other: 'what', value: 'value'}];
+  t.deepEqual(actual, expected); 
+})
+
+const newLineCsv = `title,other,values
+"Hello\nworld",what,value`;
+
+test.skip('should parse quoted newline correctly', async t => {
+  t.plan(1);
+  return t.pass();
+  const actual = await csvParser(newLineCsv);
+  const expected = [{title: 'Hello\nworld', other: 'what', values: 'values'}];
+  t.deepEqual(actual, expected);
+});
